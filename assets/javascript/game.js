47420dd03164987310e2e2ef503e6f752438f4f2
david sipala <davidsipala@MacBook-Pro-6.local>
2018-02-15 15:09:30 -0600

    second push

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+
+
+
+
+var cars = ["BMW", "Volvo", " Saab", " Ford", "Audi", "Opel", "Fiat", 
+"Renault", "Chevrolet", "Cadillac", "Ferrari", 
+"Toyota", " Subaru", "Pontiac", "Bugatti", "Porsche", "Mercedes Benz", 
+"Volkswagen", "Auto Union", "Alfa Romeo", " Tesla", "Ducati", "De Tomaso", 
+"Pagani", "Honda", " Datsun", "Suzuki", "Seat"]
+console.log(cars);
+
+for (i=0; 1<28; i++);
+console.log(i+1);
+
