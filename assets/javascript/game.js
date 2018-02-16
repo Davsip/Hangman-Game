a8eff8ba0206343236380e2e72c04e3e21824dba
david sipala <davidsipala@MacBook-Pro-6.local>
2018-02-16 07:35:37 -0600

    cuasi

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,7 +9,7 @@ var cars = ["BMW", "Volvo", " Saab", " Ford", "Audi", "Opel", "Fiat",
 "Pagani", "Honda", " Datsun", "Suzuki", "Seat"]
 console.log("cars");
 
-var alphabet = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 
 var gameStarted = false;
